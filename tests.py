0ed252b8cd648b19f4b5439586c375ee01bdf9f1
Michael Klich <michal@michalklich.com>
2016-03-14 22:13:00 +0000

    Add simple tests, add dependencies

---
+++ tests.py
@@ -0,0 +1,16 @@
+from click.testing import CliRunner
+
+from main import main
+
+
+def test_arg_part_missing():
+    runner = CliRunner()
+    result = runner.invoke(main)
+    assert result.exit_code == 2
+    assert 'Missing argument "part"' in result.output
+
+
+def test_arg_part():
+    runner = CliRunner()
+    result = runner.invoke(main, ['patch'])
+    assert result.exit_code == 0
