b5420d5edb7ddd0ab927c54f28daad3f7c0dddbb
Michael Klich <michal@michalklich.com>
2016-03-25 16:49:53 +0000

    Add tox, simple setup.

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup, find_packages
+
+
+setup(name='bamp',
+      packages=find_packages('bamp'),
+      license='MIT')
--- tests/test_interface.py
+++ tests/test_interface.py
@@ -1,6 +1,6 @@
 from click.testing import CliRunner
 
-from main import main
+from bamp.main import main
 
 
 def test_arg_part_missing():
