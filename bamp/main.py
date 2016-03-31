cccfe00aa102fd9e61d7dac9241b9ca23e8eb448
Michael Klich <michal@michalklich.com>
2016-03-31 22:55:53 +0100

    Add basic opts and args, basic use case.

--- bamp/main.py
+++ bamp/main.py
@@ -1,4 +1,11 @@
-def main():
+import click
+
+
+@click.command()
+@click.option('current_version', '--current_version')
+@click.argument('part', type=click.Choice(['patch', 'minor', 'major']))
+@click.argument('file_', nargs=-1, type=click.Path(exists=True))
+def main(current_version, part, file_):
     pass
 
 if __name__ == '__main__':
--- tests/test_interface.py
+++ tests/test_interface.py
@@ -14,3 +14,26 @@ def test_arg_part():
     runner = CliRunner()
     result = runner.invoke(main, ['patch'])
     assert result.exit_code == 0
+
+
+def test_arg_unsupported_part():
+    runner = CliRunner()
+    result = runner.invoke(main, ['foobar'])
+    assert result.exit_code == 1
+    assert 'Invalid value for "part"' in result.output
+
+
+def test_arg_part_with_file():
+    runner = CliRunner()
+    result = runner.invoke(main, ['major', 'setup.py'])
+    assert result.exit_code == 0
+    assert 'setup.py' in result.output
+
+
+def test_arg_part_with_two_files():
+    runner = CliRunner()
+    result = runner.invoke(main, ['major', 'setup.py', 'tox.ini'])
+    assert result.exit_code == 0
+    assert 'setup.py' in result.output
+    assert 'tox.ini' in result.output
+
