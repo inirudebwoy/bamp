5e4a18635bd7240c2d7a9b118bcac086c982b2ca
Michael Klich <michal@michalklich.com>
2016-04-02 23:28:37 +0100

    Add version parsing

--- bamp/engine.py
+++ bamp/engine.py
@@ -3,4 +3,32 @@ TODO: create new option type to make sure that it is in correct format?
       this would move validation off the functions into the interface, where
       it should be.
 
+TODO: meta and pre-releases
+
 """
+from collections import namedtuple
+
+VERSION_PARTS = ('major', 'minor', 'patch')
+SplitVersion = namedtuple('SplitVersion', VERSION_PARTS)
+
+VERSION_SEPARATOR = '.'
+
+
+def split_version(version):
+    int_list = map(int, version.split(VERSION_SEPARATOR))
+    return SplitVersion(*int_list)
+
+
+def join_version(version_list):
+    str_list = map(str, version_list)
+    return VERSION_SEPARATOR.join(str_list)
+
+
+def bamp_version(version, part):
+    try:
+        current_value = getattr(version, part)
+    except AttributeError:
+        # TODO: raise own exception?
+        return None
+
+    return version._replace(**{part: current_value + 1})
--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,14 +1,17 @@
-from collections import namedtuple
-
-from bamp.engine import split_version, join_version, bamp_version
-
-SplitVersion = namedtuple('SplitVersion', ['major', 'minor', 'patch'])
+from bamp.engine import split_version, join_version, bamp_version, SplitVersion
 
 
-def test_split():
+def test_split_simple():
     result = split_version('0.0.1')
     assert result.major == 0
     assert result.minor == 0
+    assert result.patch == 1
+
+
+def test_split_complex():
+    result = split_version('1.30.1')
+    assert result.major == 1
+    assert result.minor == 30
     assert result.patch == 1
 
 
@@ -16,5 +19,17 @@ def test_join():
     assert join_version([0, 0, 1]) == '0.0.1'
 
 
-def test_bamp_version():
-    assert bamp_version(SplitVersion(0, 0, 1), 'part') == SplitVersion(0, 0, 2)
+def test_bamp_patch():
+    assert bamp_version(SplitVersion(0, 0, 1), 'patch') == SplitVersion(0, 0, 2)
+
+
+def test_bamp_minor():
+    assert bamp_version(SplitVersion(0, 0, 9), 'minor') == SplitVersion(0, 1, 0)
+
+
+def test_bamp_major_with_minor():
+    assert bamp_version(SplitVersion(0, 1, 1), 'major') == SplitVersion(1, 0, 0)
+
+
+def test_bamp_major():
+    assert bamp_version(SplitVersion(2, 0, 1), 'major') == SplitVersion(3, 0, 0)
