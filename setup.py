43751db1db3774366fdfbfbcd9033704a8f1baeb
Michael Klich <michal@michalklich.com>
2016-04-03 12:29:48 +0100

    Update package info

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
+# -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
 
 setup(name='bamp',
+      version='0.0.1',
       packages=find_packages('bamp'),
-      license='MIT')
+      description='Bump version according to semantic versioning',
+      author='Michał Klich',
+      author_email='michal@michalklich.com',
+      url='https://github.com/inirudebwoy/bamp',
+      license='MIT',
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Console',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 2',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.4',
+                   'Topic :: Software Development :: Build Tools'])
