a18479ada39401e39b6fc63e7fbaffc775a1d88c
Michael Klich <michal@michalklich.com>
2016-04-03 09:13:38 +0100

    Add docs and notes

--- bamp/engine.py
+++ bamp/engine.py
@@ -15,17 +15,43 @@ VERSION_SEPARATOR = '.'
 
 
 def split_version(version):
+    """Split incoming version into namedtuple
+
+    :param version: version in form of comma separated string
+    :type version: str
+    :returns: version in form of namedtuple (keys: ('major', 'minor', 'patch'))
+    :rtype: namedtuple SplitVersion
+
+    """
     int_list = map(int, version.split(VERSION_SEPARATOR))
     return SplitVersion(*int_list)
 
 
 def join_version(version_list):
+    """Join version in form namedtuple into string
+
+    :param version_list: version elements
+    :type version_list: iterable
+    :returns: version as string, comma separated
+    :rtype: str
+
+    """
     str_list = map(str, version_list)
     return VERSION_SEPARATOR.join(str_list)
 
 
+# TODO: this should be a private function
 def bamp_version(version, part):
+    """Bump version according to semantic versioning
 
+    :param version: version to bump
+    :type version: iterable
+    :param part: name of the part to be bumped
+    :type part: str
+    :returns: bumped version
+    :rtype: namedtuple SplitVersion
+
+    """
     # TODO: will this be called from other places than UI?
     #       this validation could be moved outside
     try:
