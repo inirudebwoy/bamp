00755201473056dec2d33e05d9e244f10a3674ab
Michael Klich <michal@michalklich.com>
2016-06-24 23:24:10 +0100

    Add callbacks and logs module

---
+++ bamp/logs.py
@@ -0,0 +1,38 @@
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class ExceptionFilter(logging.Filter):
+    def __init__(self, debug=None):
+        self.debug = debug
+
+    def filter(self, record):
+        if self.debug:
+            return True
+
+        # clear exceptions when not in debug
+        if record.levelname == 'ERROR' and record.exc_info:
+            record.exc_info = None
+        return True
+
+LOGGING = {
+    'version': 1,
+    'filters': {
+        'exc_filter': {
+            '()': ExceptionFilter,
+            'debug': False
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'filters': ['exc_filter']
+        }
+    },
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['console']
+    },
+}
--- bamp/main.py
+++ bamp/main.py
@@ -1,82 +1,30 @@
 '''
 TODO: is newline on windows different for python?
-TODO: pass config as an option, see aliases.py from click examples
 TODO: dry-run? use logging for printing
 
 '''
 import logging
 import logging.config
-import sys
 
 import click
 
-from bamp.config import find_config, prepare_config, make_default_map
 from bamp.engine import bamp_version
 from bamp.persistence import bamp_files
-from bamp.exc import ErrorConfigParsing
+from bamp.callbacks import enable_debug, read_config, required
 
 
-DEBUG = False
+logger = logging.getLogger(__name__)
 
-
-class MyFilter(logging.Filter):
-    def __init__(self, debug=None):
-        self.debug = debug
-
-    def filter(self, record):
-        # TODO: Debug mode here should print everything
-        # I want to display exception messages to a user but with no traceback
-        # with filter on a handler I should be able to do it.
-        # pass errors and info(?) to StreamHandler, rest to null unless in Debug mode.
-        # than everything is getting printed out.
-        if self.debug:
-            return True
-
-        if record.levelname == 'ERROR' and record.exc_info:
-            record.exc_info = None
-        return True
-
-LOGGING = {
-    'version': 1,
-    'filters': {
-        'myfilter': {
-            '()': MyFilter,
-            'debug': DEBUG
-        }
-    },
-    'handlers': {
-        'console': {
-            'class': 'logging.StreamHandler',
-            'filters': ['myfilter']
-        }
-    },
-    'root': {
-        'level': 'DEBUG',
-        'handlers': ['console']
-    },
-}
-
-
-def required(ctx, param, value):
-    """Make sure that option passed in has value
-
-    Some options can be passed from the command line, using flags, like
-    --version or be retrieved from config file. Function makes sure
-    that passed option has a value.
-
-    """
-    if not value:
-        raise click.UsageError(
-            '"%(name)s" is required. Add to config or pass with %(flag)s '
-            'option.' % {'name': param.name,
-                         'flag': param.opts})
-    return value
 
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
-@click.option('--debug', is_flag=True)
+@click.option('--debug', help='Enable debug flag.', is_flag=True,
+              expose_value=False, callback=enable_debug, is_eager=True)
+@click.option('--config', type=click.Path(exists=True, dir_okay=False),
+              help='Path to a config file.',
+              callback=read_config, expose_value=False)
 @click.option('-v', '--version', help='Current version of the program.',
               callback=required)
 @click.option('files', '-f', '--file',
@@ -86,23 +34,11 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
               callback=required)
 @click.argument('part', nargs=1,
                 type=click.Choice(['patch', 'minor', 'major']))
-def bamp(version, part, files, debug):
+def bamp(version, part, files):
     new_version = bamp_version(version, part)
     bamp_files(version, new_version, files)
     # TODO: VC goes here, if config is set
     click.echo(new_version)
 
 if __name__ == '__main__':
-    if '--debug' in sys.argv:
-        DEBUG = True
-        LOGGING['filters']['myfilter']['debug'] = DEBUG
-
-    logging.config.dictConfig(LOGGING)
-
-    try:
-        config = prepare_config(find_config())
-    except KeyboardInterrupt:
-        click.echo('User cancelled.')
-    except ErrorConfigParsing:
-        sys.exit(1)
-    bamp(default_map=make_default_map(config))
+    bamp()
