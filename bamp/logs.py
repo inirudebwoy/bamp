3e4094d838df26afadfa7074a16c6a21f06ba6b8
Michael Klich <michal@michalklich.com>
2016-07-17 11:14:32 +0100

    Disable printing error messages not in debug

--- bamp/logs.py
+++ bamp/logs.py
@@ -1,24 +1,20 @@
 import logging
 
 
-class ExceptionFilter(logging.Filter):
+class DebugFilter(logging.Filter):
     def __init__(self, debug=None):
         self.debug = debug
 
     def filter(self, record):
         if self.debug:
             return True
-
-        # clear exceptions when not in debug
-        if record.levelname == 'ERROR' and record.exc_info:
-            record.exc_info = None
-        return True
+        return False
 
 LOGGING = {
     'version': 1,
     'filters': {
         'exc_filter': {
-            '()': ExceptionFilter,
+            '()': DebugFilter,
             'debug': False
         }
     },
