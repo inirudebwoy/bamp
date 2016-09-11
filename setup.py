6a6b6c22e3e4f0973556d6714b22dc9735ecf1f2
Michael Klich <michal@michalklich.com>
2016-09-11 12:48:34 +0000

    Bamp version: 0.1.0 -> 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bamp',
-    version='0.1.0',
+    version='0.1.1',
     install_requires=['Click'],
     entry_points='''
       [console_scripts]
