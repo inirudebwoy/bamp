c6cfd69ee5e156af64632ea4e85caa5eb098bcc5
Michał Klich <michal@klich.dev>
2020-03-28 15:23:04 +0100

    Add -n switch and use stdout

--- bamp/main.py
+++ bamp/main.py
@@ -1,9 +1,9 @@
-'''
+"""
 TODO: is newline on windows different for python?
 TODO: dry-run? use logging for printing
 TODO: treat PART as a custom command
-       http://click.pocoo.org/6/commands/#custom-multi-commands ?
-'''
+    http://click.pocoo.org/6/commands/#custom-multi-commands ?
+"""
 import logging
 import logging.config
 
@@ -13,61 +13,75 @@ from bamp.config import add_config, get_root_path
 from bamp.engine import bamp_version
 from bamp.helpers import docs
 from bamp.helpers.callbacks import enable_debug, read_config, required
-from bamp.helpers.ui import ok_exit, verify_response
+from bamp.helpers.ui import machine_out, verify_response
 from bamp.persistence import bamp_files
-from bamp.vcs import (create_commit, create_tag, is_tree_clean, make_message,
-                      make_tag_name)
+from bamp.vcs import (
+    create_commit,
+    create_tag,
+    is_tree_clean,
+    make_message,
+    make_tag_name,
+)
 
-logger = logging.getLogger('bamp')
+logger = logging.getLogger("bamp")
 
-CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
+CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 
 @click.command(context_settings=CONTEXT_SETTINGS)
+@click.option("-n", "new_line", help="Don't print new line", is_flag=True, default=True)
 @click.option(
-    '--debug',
+    "--debug",
     help=docs.DEBUG_OPTION_HELP,
     is_flag=True,
     expose_value=False,
     callback=enable_debug,
-    is_eager=True)
+    is_eager=True,
+)
 @click.option(
-    '--config',
-    type=click.Path(
-        exists=True, dir_okay=False),
+    "--config",
+    type=click.Path(exists=True, dir_okay=False),
     help=docs.CONFIG_OPTION_HELP,
-    callback=read_config)
+    callback=read_config,
+)
+@click.option("-v", "--version", help=docs.VERSION_OPTION_HELP, callback=required)
 @click.option(
-    '-v', '--version', help=docs.VERSION_OPTION_HELP, callback=required)
-@click.option(
-    'files',
-    '-f',
-    '--file',
+    "files",
+    "-f",
+    "--file",
     help=docs.FILES_OPTION_HELP,
     type=click.Path(exists=True),
-    multiple=True)
-@click.option('vcs', '-V', '--vcs', help=docs.VCS_OPTION_HELP)
-@click.option('allow_dirty', '-a', '--allow-dirty', is_flag=True)
+    multiple=True,
+)
+@click.option("vcs", "-V", "--vcs", help=docs.VCS_OPTION_HELP)
+@click.option("allow_dirty", "-a", "--allow-dirty", is_flag=True)
 @click.option(
-    'commit',
-    '-c',
-    '--commit',
-    is_flag=True,
-    help=docs.COMMIT_FLAG_OPTION_HELP)
-@click.option('message', '-m', '--message', help=docs.MESSAGE_OPTION_HELP)
+    "commit", "-c", "--commit", is_flag=True, help=docs.COMMIT_FLAG_OPTION_HELP
+)
+@click.option("message", "-m", "--message", help=docs.MESSAGE_OPTION_HELP)
+@click.option("tag", "-t", "--tag", is_flag=True, help=docs.TAG_FLAG_OPTION_HELP)
 @click.option(
-    'tag', '-t', '--tag', is_flag=True, help=docs.TAG_FLAG_OPTION_HELP)
-@click.option(
-    'tag_name',
-    '-T',
-    '--tag-name',
+    "tag_name",
+    "-T",
+    "--tag-name",
     help=docs.TAG_NAME_OPTION_HELP,
-    metavar=docs.TAG_NAME_OPTION_METAVAR)
-@click.argument(
-    'part', nargs=1, type=click.Choice(['patch', 'minor', 'major']))
+    metavar=docs.TAG_NAME_OPTION_METAVAR,
+)
+@click.argument("part", nargs=1, type=click.Choice(["patch", "minor", "major"]))
 @add_config
-def bamp(version, part, files, vcs, allow_dirty, commit, message, config, tag,
-         tag_name):
+def bamp(
+    new_line,
+    version,
+    part,
+    files,
+    vcs,
+    allow_dirty,
+    commit,
+    message,
+    config,
+    tag,
+    tag_name,
+):
     root_path = get_root_path()
     sanity_checks(root_path)
 
@@ -81,7 +95,7 @@ def bamp(version, part, files, vcs, allow_dirty, commit, message, config, tag,
         tag_message = make_tag_name(tag_name, new_version)
         create_tag(vcs, commit_sha1, tag_message)
 
-    ok_exit('New version: {0}'.format(new_version))
+    machine_out(new_version)
 
 
 @verify_response
@@ -95,7 +109,7 @@ def sanity_checks(root_path):
 
     """
     ctx = click.get_current_context()
-    if ctx.params.get('commit'):
-        is_tree_clean(ctx.params.get('vcs'), root_path)
+    if ctx.params.get("commit"):
+        is_tree_clean(ctx.params.get("vcs"), root_path)
 
     return True, []
