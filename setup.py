7d7ba881be91716d7f9712f8393727246f8a5a67
Michał Klich <michal@klichx.dev>
2020-09-09 09:48:48 +0000

    Bamp version: 0.3.0 -> 1.0.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="bamp",
-    version="0.3.0",
+    version="1.0.0",
     install_requires=["Click", "dulwich", "six"],
     entry_points="""
         [console_scripts]
