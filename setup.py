8b1402c9429fe59dc1c1d1999e311f107d7fe7d7
Michael Klich <michal@klich.dev>
2020-09-09 09:32:07 +0000

    Issue 15 (#16)
    * Add tests covering bug #15
    
    * Fixes #15

--- setup.py
+++ setup.py
@@ -4,36 +4,39 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+with io.open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='bamp',
-    version='0.3.0',
-    install_requires=['Click', 'dulwich', 'six'],
-    entry_points='''
+    name="bamp",
+    version="0.3.0",
+    install_requires=["Click", "dulwich", "six"],
+    entry_points="""
       [console_scripts]
       bamp=bamp.main:bamp
-      ''',
+      """,
     packages=find_packages(),
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     include_package_data=True,
     zip_safe=True,
-    description='Bamp version according to semantic versioning',
-    author='Michał Klich',
-    author_email='michal@michalklich.com',
-    url='https://gitlab.com/the_speedball/bamp',
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest'],
-    license='MIT',
-    classifiers=['Development Status :: 4 - Beta', 'Environment :: Console',
-                 'License :: OSI Approved :: MIT License',
-                 'Operating System :: OS Independent',
-                 'Programming Language :: Python :: 2',
-                 'Programming Language :: Python :: 2.7',
-                 'Programming Language :: Python :: 3',
-                 'Programming Language :: Python :: 3.5',
-                 'Programming Language :: Python :: 3.6',
-                 'Programming Language :: Python :: 3.7',
-                 'Topic :: Software Development :: Build Tools'])
+    description="Bamp version according to semantic versioning",
+    author="Michał Klich",
+    author_email="michal@michalklich.com",
+    url="https://gitlab.com/the_speedball/bamp",
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    license="MIT",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Build Tools",
+    ],
+)
--- tests/test_interface.py
+++ tests/test_interface.py
@@ -10,14 +10,14 @@ def test_arg_part_missing():
     with runner.isolated_filesystem():
         result = runner.invoke(bamp)
         assert result.exit_code == 2
-        assert 'Missing argument "[patch|minor|major|current]"' in result.output
+        assert "Missing argument '[patch|minor|major|current]'" in result.output
 
 
 def test_arg_part_no_version():
     """bamp patch"""
     runner = CliRunner()
     with runner.isolated_filesystem():
-        result = runner.invoke(bamp, ['patch'])
+        result = runner.invoke(bamp, ["patch"])
         assert result.exit_code == 2
         assert '"version" is required.' in result.output
 
@@ -26,7 +26,7 @@ def test_arg_part_with_version():
     """bamp patch -v 0.0.1"""
     runner = CliRunner()
     with runner.isolated_filesystem():
-        result = runner.invoke(bamp, ['patch', '-v', '0.0.1'])
+        result = runner.invoke(bamp, ["patch", "-v", "0.0.1"])
         assert result.exit_code == 0
 
 
@@ -34,20 +34,18 @@ def test_arg_part_with_version_with_nonexisting_file():
     """bamp patch -v 0.0.1 -f version.ini"""
     runner = CliRunner()
     with runner.isolated_filesystem():
-        result = runner.invoke(bamp,
-                               ['patch', '-v', '0.0.1', '-f', 'version.ini'])
+        result = runner.invoke(bamp, ["patch", "-v", "0.0.1", "-f", "version.ini"])
         assert result.exit_code == 2
-        assert '"version.ini" does not exist' in result.output
+        assert "'version.ini' does not exist" in result.output
 
 
 def test_arg_part_with_version_with_existing_file():
     """bamp patch -v 0.0.1 -f version.ini"""
     runner = CliRunner()
     with runner.isolated_filesystem():
-        with open('version.ini', 'w') as v:
-            v.write('0.0.1')
-        result = runner.invoke(bamp,
-                               ['patch', '-v', '0.0.1', '-f', 'version.ini'])
+        with open("version.ini", "w") as v:
+            v.write("0.0.1")
+        result = runner.invoke(bamp, ["patch", "-v", "0.0.1", "-f", "version.ini"])
 
         assert result.exit_code == 0
 
@@ -56,19 +54,20 @@ def test_arg_unsupported_part():
     """bamp foobar"""
     runner = CliRunner()
     with runner.isolated_filesystem():
-        result = runner.invoke(bamp, ['foobar'])
+        result = runner.invoke(bamp, ["foobar"])
         assert result.exit_code == 2
-        assert 'Invalid value for "[patch|minor|major|current]"' in result.output
+        assert "Invalid value for '[patch|minor|major|current]'" in result.output
 
 
 def test_with_default_commit_no_vcs():
     """bamp patch -v 0.0.1 -f version.ini -c"""
     runner = CliRunner()
     with runner.isolated_filesystem():
-        with open('version.ini', 'w') as v:
-            v.write('0.0.1')
+        with open("version.ini", "w") as v:
+            v.write("0.0.1")
         result = runner.invoke(
-            bamp, ['patch', '-v', '0.0.1', '-f', 'version.ini', '-c'])
+            bamp, ["patch", "-v", "0.0.1", "-f", "version.ini", "-c"]
+        )
         assert result.exit_code == 1
 
 
@@ -76,11 +75,12 @@ def test_default_tag_default_commit_with_vcs():
     """bamp patch -v 0.0.1 -f version.ini -ct"""
     runner = CliRunner()
     with runner.isolated_filesystem():
-        git_repo_fixture('.')
-        with open('version.ini', 'w') as v:
-            v.write('0.0.1')
+        git_repo_fixture(".")
+        with open("version.ini", "w") as v:
+            v.write("0.0.1")
         result = runner.invoke(
-            bamp, ['patch', '-v', '0.0.1', '-f', 'version.ini', '-c', '-t'])
+            bamp, ["patch", "-v", "0.0.1", "-f", "version.ini", "-c", "-t"]
+        )
         assert result.exit_code == 0
 
 
@@ -88,11 +88,21 @@ def test_custom_tag_default_commit_with_vcs():
     """bamp patch -v 0.0.1 -f version.ini -c -t -T tag-{new_version}"""
     runner = CliRunner()
     with runner.isolated_filesystem():
-        git_repo_fixture('.')
-        with open('version.ini', 'w') as v:
-            v.write('0.0.1')
-        result = runner.invoke(bamp, [
-            'patch', '-v', '0.0.1', '-f', 'version.ini', '-c', '-t', '-T',
-            'tag-{new_version}'
-        ])
+        git_repo_fixture(".")
+        with open("version.ini", "w") as v:
+            v.write("0.0.1")
+        result = runner.invoke(
+            bamp,
+            [
+                "patch",
+                "-v",
+                "0.0.1",
+                "-f",
+                "version.ini",
+                "-c",
+                "-t",
+                "-T",
+                "tag-{new_version}",
+            ],
+        )
         assert result.exit_code == 0
